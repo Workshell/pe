4de6f1a915e4d4793d9f3ecb02cd31199ce7467a
Lloyd Kinsella <lkinsella@uk.imshealth.com>
2018-09-28 15:25:24 +0100

    More cursor work.

---
+++ src/Workshell.PE.Resources/Graphics/CursorInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Workshell.PE.Resources.Graphics
+{
+    public sealed class CursorInfo
+    {
+        internal CursorInfo(CursorResource cursor, ResourceLanguage language, ushort hotspotX, ushort hotspotY, ushort width, ushort height, byte colors, byte[] dib, bool isPNG)
+        {
+            Cursor = cursor;
+            Language = language;
+            Hotspot = new Point(hotspotX, hotspotY);
+            Size = new Size(width, height);
+            Colors = colors;
+            DIB = dib;
+            IsPNG = isPNG;
+        }
+
+        #region Properties
+
+        public CursorResource Cursor { get; }
+        public ResourceLanguage Language { get; }
+        public Point Hotspot { get; }
+        public Size Size { get; }
+        public byte Colors { get; }
+        public byte[] DIB { get; }
+        public bool IsPNG { get; }
+
+        #endregion
+    }
+}
---
+++ src/Workshell.PE.Resources/Graphics/CursorResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshell.PE.Resources.Graphics
+{
+    public sealed class CursorResource : Resource
+    {
+        public CursorResource(PortableExecutableImage image, ResourceType type, ResourceDirectoryEntry entry, ResourceId id) : base(image, type, entry, id)
+        {
+        }
+    }
+}
