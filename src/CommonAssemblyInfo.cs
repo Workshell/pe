6b583a6903267ecea5ea4c74148f91e0cd2c72ff
Lloyd Kinsella <lloydkinsella@gmail.com>
2018-09-28 19:24:58 +0100

    Fixed up common version information etc.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Workshell Ltd")]
+[assembly: AssemblyProduct(".NET PE Class Library")]
+[assembly: AssemblyCopyright("Copyright © Workshell Ltd")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
---
+++ src/Workshell.PE.Resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Workshell.PE.Resources")]
+[assembly: AssemblyDescription("A set of classes for dealing with resources within a PE file.")]
